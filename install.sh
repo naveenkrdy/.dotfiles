ef6538f4238eaaa9abcf4cf1f481dc4a8dc0459d
naveenkrdy <naveenk2908@gmail.com>
2020-07-06 01:19:38 +0530

    sync changes

--- install.sh
+++ install.sh
@@ -2,11 +2,7 @@
 # dotFiles
 # naveenkrdy (28/12/19)
 
-#check OS
-echo "$OSTYPE" | grep -q darwin
-[[ $? != 0 ]] && echo "Unsupported OS" && exit 1
-
-export MAIN_DIR="$(dirname ${0})"
+export MAIN_DIR="$(dirname "${0}")"
 export WALLPAPER="Beach"
 export TERMINAL="Broadcast"
 export SCREENSAVER=""
@@ -15,26 +11,21 @@ italic="\x1b[3m"
 normal="\033[0;0;39m"
 
 function kill_them() {
-	for app in "Activity Monitor" \
-		"cfprefsd" \
-		"Safari" \
-		"Disk Utility" \
-		"Dock" \
-		"Finder" \
-		"TextEdit" \
-		"System Prefrences" \
-		"SystemUIServer"; do
-		killall ${app} &>/dev/null
-	done
+    for app in "Activity Monitor" \
+        "cfprefsd" \
+        "Safari" \
+        "Disk Utility" \
+        "Dock" \
+        "Finder" \
+        "TextEdit" \
+        "System Prefrences" \
+        "SystemUIServer"; do
+        killall ${app} &>/dev/null
+    done
 }
-function prevent_sleep() {
-	if [[ ${1} != "kill" ]]; then
-		caffeinate -id &
-		sleep_pid=${!}
-	else
-		kill ${sleep_pid} &>/dev/null
-	fi
-}
+
+#Start
+[[ "$(uname)" != "Darwin" ]] && echo "Unsupported OS" && exit 1
 
 printf '\033[8;60;200t' && clear
 echo
@@ -47,21 +38,20 @@ echo
 
 sudo -v
 while true; do
-	sudo -n true
-	sleep 10000
-	kill -0 ${$} || exit
+    sudo -n true
+    sleep 10000
+    kill -0 ${$} || exit
 done 2>/dev/null &
 
-prevent_sleep
+caffeinate -id &
+sleep_pid=${!}
 
-# Start
 source ${MAIN_DIR}/brew/_setup.sh
 source ${MAIN_DIR}/git/_setup.sh
 source ${MAIN_DIR}/vim/_setup.sh
 source ${MAIN_DIR}/zsh/_setup.sh
 source ${MAIN_DIR}/macos/_setup.sh
-# End
 
-prevent_sleep kill
+kill ${sleep_pid} &>/dev/null
 
 echo "Finished !"
