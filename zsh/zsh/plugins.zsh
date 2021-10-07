c226cd31014e8b47509b40f5397197414d1c039c
naveenkrdy <naveenk2908@gmail.com>
2021-10-07 12:03:35 +0530

    fix antigen and update prompt style

--- zsh/zsh/plugins.zsh
+++ zsh/zsh/plugins.zsh
@@ -1,14 +1,10 @@
-# Antigen setup
-
-function _plugin_settings {
+antigen_plugin_settings() {
 	# OMZ Settings
 	DISABLE_AUTO_TITLE="true"
 	HIST_STAMPS="dd.mm.yyyy"
 	# Spaceship theme settings
 	SPACESHIP_PROMPT_ORDER=(
-		dir # Current directory section
 		# host # Hostname section
-		git # Git section (git_branch + git_status)
 		# hg        # Mercurial section (hg_branch  + hg_status)
 		# package   # Package version
 		# gradle    # Gradle section
@@ -34,46 +30,45 @@ function _plugin_settings {
 		# ember     # Ember.js section
 		# kubectl   # Kubectl context section
 		# terraform # Terraform workspace section
-		exec_time # Execution time
-		line_sep  # Line break
-		vi_mode   # Vi-mode indicator
-		user      # Username section
-		jobs      # Background jobs indicator
-		char      # Prompt character
+		# line_sep  # Line break
+		# vi_mode   # Vi-mode indicator
+		# user      # Username section
+		char # Prompt character
 	)
 	SPACESHIP_RPROMPT_ORDER=(
-		battery   # Battery level and status
 		exit_code # Exit code section
-		time      # Time stamps section
+		exec_time # Execution time
+		jobs      # Background jobs indicator
+		dir       # Current directory section
+		git       # Git section (git_branch + git_status)
+		# battery   # Battery level and status
+		# time      # Time stamps section
 	)
+	# SPACESHIP_PROMPT_ADD_NEWLINE="false"
+	SPACESHIP_PROMPT_FIRST_PREFIX_SHOW="true"
 	SPACESHIP_CHAR_COLOR_SUCCESS="yellow"
 	SPACESHIP_CHAR_SUFFIX=" "
-	# SPACESHIP_CHAR_SYMBOL=" \uf0e7 "
 	SPACESHIP_CHAR_SYMBOL=" \uf296 "
 	SPACESHIP_DIR_LOCK_SYMBOL=" \uf023 "
-	# SPACESHIP_DIR_PREFIX=" \uf52d  "
-	SPACESHIP_DIR_PREFIX=" \uf01c  "
-	SPACESHIP_DIR_SHOW="true"
+	SPACESHIP_DIR_PREFIX="\uf01c "
 	SPACESHIP_EXIT_CODE_SHOW="true"
-	SPACESHIP_PROMPT_FIRST_PREFIX_SHOW="true"
-	SPACESHIP_TIME_PREFIX=""
-	SPACESHIP_TIME_SHOW="true"
-	SPACESHIP_TIME_SUFFIX=""
-	SPACESHIP_USER_PREFIX=" "
-	SPACESHIP_USER_SUFFIX=""
+	# SPACESHIP_EXEC_TIME_PREFIX=""
+	SPACESHIP_EXEC_TIME_PREFIX="\ufc23"
+	SPACESHIP_EXEC_TIME_SUFFIX=" "
 
 }
 
-function _antigen_download {
+antigen_download() {
 	# Download antigen
 	_check_internet || return 1
 	curl -L git.io/antigen >${zsh_dir}/antigen.zsh
 }
-function _antigen_load {
-	# Load antigen
+antigen_load() {
+	# Source antigen
+	autoload antigen
 	source ${zsh_dir}/antigen.zsh
 	# Load plugin settings
-	_plugin_settings
+	antigen_plugin_settings
 	# Use OMZ framework
 	antigen use oh-my-zsh
 	# Load plugins from OMZ
@@ -97,9 +92,10 @@ function _antigen_load {
 
 # Change default antigen directory
 ADOTDIR="$zsh_dir/antigen"
+
 if [[ ! -f ${zsh_dir}/antigen.zsh ]]; then
 	echo "Setting up antigen..."
-	_antigen_download && _antigen_load
+	antigen_download && antigen_load
 else
-	_antigen_load
+	antigen_load
 fi
